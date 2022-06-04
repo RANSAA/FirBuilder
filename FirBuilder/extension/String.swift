6dbf33b3b515ae4fe03647754cc3d8a1cb4e8d9a
sayaDev <1352892108@qq.com>
2022-06-04 23:23:37 +0800

    no message

--- FirBuilder/extension/String.swift
+++ FirBuilder/extension/String.swift
@@ -7,7 +7,7 @@
 
 import Cocoa
 
-//subString
+//MARK: subString
 extension String {
     // 截取字符串：从index到结束处
     // - Parameter index: 开始索引
@@ -94,7 +94,7 @@ extension String {
  }
 
 
-//NSRange 与 Range互相转换
+// MARK: NSRange 与 Range互相转换
 extension String{
 
     //Range转换为NSRange
@@ -118,6 +118,7 @@ extension String{
 }
 
 
+// MARK: File Name
 extension String{
 
     //文件扩展名
@@ -193,4 +194,91 @@ extension String{
         fclose(fp)
         return resultStr
     }
+}
+
+
+
+// MARK: String.Encoding编码扩展
+extension String.Encoding{
+    //GBK原始获取方式
+    //let cfEnc = CFStringEncodings.GB_18030_2000
+    //let enc = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(cfEnc.rawValue))
+    //let gbk = String.Encoding.init(rawValue: enc)
+    //enc的值为：2147485234，所以可以直接使用这个值创建GBK编码
+    //GBK编码, 使用GB18030是因为它向下兼容GBK
+    public static let gbk: String.Encoding = .init(rawValue: 2147485234)
+
+}
+
+
+
+
+// MARK: Base64 safe url
+extension String{
+
+    //Base64 编码
+    public func encBase64() -> String{
+        var base64String = ""
+        if let data = self.data(using: .utf8) {
+            base64String = data.base64EncodedString(options: Data.Base64EncodingOptions.init(rawValue: 0))
+        }else{
+            print("⚠️ Base64 编码失败!")
+        }
+        return base64String
+    }
+
+    //Base64 解码
+    public func decBase64() -> String{
+        var decodeString = ""
+        if let deData = Data(base64Encoded: self, options: .init(rawValue: 0)) {
+            decodeString = String(data: deData, encoding: .utf8) ?? decodeString
+        }else{
+            print("⚠️ Base64 解码失败!")
+        }
+        return decodeString
+    }
+
+
+
+    //Base64 url safe 编码
+    public func encBase64WebSafe() ->String{
+        var base64String = ""
+        if let data = self.data(using: .utf8) {
+            base64String = data.base64EncodedString(options: Data.Base64EncodingOptions.init(rawValue: 0))
+            // %替换为_
+            base64String = base64String.replacingOccurrences(of: "%", with: "_")
+            // =替换为空
+            base64String = base64String.replacingOccurrences(of: "=", with: "")
+            // +替换为—
+            base64String = base64String.replacingOccurrences(of: "+", with: "-")
+            // /替换为_
+            base64String = base64String.replacingOccurrences(of: "/", with: "_")
+        }else{
+            print("⚠️ Base64 Url Safe 编码失败!")
+//            LogManager.log("⚠️ Base64 Url Safe 编码失败!")
+        }
+        return base64String
+    }
+
+    //Base64 url safe 解码
+    public func decBase64WebSafe() ->String{
+        var decodeString = ""
+        // -替换为+
+        var base64Str = self.replacingOccurrences(of: "-", with: "+")
+        // _替换为/
+        base64Str = base64Str.replacingOccurrences(of: "_", with: "/")
+        let mod4 = base64Str.count % 4
+        if mod4 > 0 {
+                let appStr = ("====" as NSString).substring(to: (4 - mod4))
+            base64Str += appStr
+        }
+        if let data = Data(base64Encoded: base64Str, options: .init(rawValue: 0)) {
+            decodeString = String(data: data, encoding: .utf8) ?? decodeString
+        }else{
+            print("⚠️ Base64 Url Safe 解码失败!     original:\(self)")
+//            LogManager.log("⚠️ Base64 Url Safe 解码失败!     original:\(self)")
+        }
+        return decodeString
+    }
+
 }
